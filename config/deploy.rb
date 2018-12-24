c897ce559dc48cebc27f912130eb5efb1ec86477
boopathi <boopathimca2013@gmail.com>
2018-12-24 13:40:39 +0530

    commit gem lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "ethics"
-set :repo_url, "git@github.com:boopathimca2013/apps.git"
+set :repo_url, "git@github.com:boopathimca2013/ethics.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -40,14 +40,14 @@ set :repo_url, "git@github.com:boopathimca2013/apps.git"
 
 
 #set :repo_url, 'git@github.com:devdatta/contactbook.git' # Edit this to match your repository
-set :branch, :master
-set :deploy_to, '/home/deploy/contactbook'
+set :branch, :dec_16
+set :deploy_to, '/home/deploy/ethics'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-set :rvm_ruby_version, 'jruby-1.7.19' # Edit this if you are using MRI Ruby
+set :rvm_ruby_version, 'ruby-2.3.8' # Edit this if you are using MRI Ruby
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '18.191.206.75', user: 'deploy', roles: %w{web app db}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
